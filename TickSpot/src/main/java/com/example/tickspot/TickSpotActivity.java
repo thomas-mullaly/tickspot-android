aa96978cae76697034b8426c7613070ca050c34a
Thomas Mullaly <tmullaly88@gmail.com>
2013-05-24 23:09:01 -0400

    Fixed some imports

--- TickSpot/src/main/java/com/example/tickspot/TickSpotActivity.java
+++ TickSpot/src/main/java/com/example/tickspot/TickSpotActivity.java
@@ -1,16 +1,12 @@
 package com.example.tickspot;
 
-
 import android.app.ActionBar;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
