5514baf53c9529ec2f207246094b2cfc35329b37
Thomas Mullaly <tmullaly88@gmail.com>
2013-05-31 21:50:53 -0400

    New project using IntelliJ. Thanks Android Studio...

---
+++ src/com/tronix/tickspot/TickSpotActivity.java
@@ -0,0 +1,15 @@
+package com.tronix.tickspot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TickSpotActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
