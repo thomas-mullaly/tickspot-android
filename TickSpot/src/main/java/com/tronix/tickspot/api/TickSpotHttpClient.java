8771d041a7ce7e7151c00f086599ff6f5bf7dffc
Thomas Mullaly <tmullaly88@gmail.com>
2013-05-31 21:44:40 -0400

    Hahahaha, line endings… Also added a few more libraries

--- TickSpot/src/main/java/com/tronix/tickspot/api/TickSpotHttpClient.java
+++
@@ -1,8 +0,0 @@
-package com.tronix.tickspot.api;
-
-import com.loopj.android.http.*;
-
-public interface TickSpotHttpClient {
-    public boolean areValidCredentials(TickSpotCredentials tickSpotCredentials);
-}
-
