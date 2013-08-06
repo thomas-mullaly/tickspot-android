3bc3d40152b7d1c28b59cc0480a11ae85d8c4ed5
Thomas Mullaly <tmullaly88@gmail.com>
2013-08-05 21:15:01 -0400

    Fixed an issue where we were trying to set the title of the actionbar too early

--- src/com/tronix/tickspot/ui/TimersFragment.java
+++ src/com/tronix/tickspot/ui/TimersFragment.java
@@ -3,6 +3,7 @@ package com.tronix.tickspot.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.*;
 import com.tronix.tickspot.R;
@@ -12,12 +13,14 @@ public class TimersFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         setHasOptionsMenu(true);
-
-        if (activity instanceof ActionBarActivity) {
-            ((ActionBarActivity)activity).getSupportActionBar().setTitle("Timers");
-        }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        ActionBarActivity activity = (ActionBarActivity)getActivity();
+        activity.getSupportActionBar().setTitle("Timers");
+    }
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
         return layoutInflater.inflate(R.layout.fragment_timers, container, false);
