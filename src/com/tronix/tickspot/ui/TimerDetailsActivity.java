c20f03bc96a2b4a31a86ffc54ce7a756514f9aea
Thomas Mullaly <tmullaly88@gmail.com>
2013-08-20 21:30:11 -0400

    Gave up with trying to display the navigation drawer on detail activities. It's not worth the effort.

--- src/com/tronix/tickspot/ui/TimerDetailsActivity.java
+++ src/com/tronix/tickspot/ui/TimerDetailsActivity.java
@@ -1,14 +1,15 @@
 package com.tronix.tickspot.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import com.tronix.tickspot.R;
 
-public class TimerDetailsActivity extends BaseDetailActivity {
+public class TimerDetailsActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         setContentView(R.layout.activity_timer_details);
-
-        super.setUpNavigationDrawer();
     }
 }
